fd35e2bc4c36ed73945cb478b3067221aae738b4
Sujal <palsujal707@gmail.com>
2026-03-23 19:17:52 +0530

    fix next config error

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
     unoptimized: true, 
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default nextConfig;
